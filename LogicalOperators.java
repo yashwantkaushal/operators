e8d865b483810b288aa41b0592e54ece018b2282
Yashwant kaushal <54852557+yashwantkaushal@users.noreply.github.com>
2020-08-23 23:18:36 +0530

    Add files via upload

---
+++ LogicalOperators.java
@@ -0,0 +1,22 @@
+package operator;
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		 
+//		int number = -4;
+//		
+//		if(number >= 1 && number <= 100) {
+//			System.out.println("Number is in the range");
+//		}
+		
+	
+	 int  grade = 11;
+	 
+	 if(!(grade == 10  || grade == 12)) {
+		 System.out.println("you can not  give bord exam now.");
+		  
+	 }
+	}
+
+}
