2f086c9d28756d4a5d45afa3061a039589398fe9
Yashwant kaushal <54852557+yashwantkaushal@users.noreply.github.com>
2020-08-23 23:17:49 +0530

    Add files via upload

---
+++ BitwiseOperators.java
@@ -0,0 +1,37 @@
+package operator;
+
+public class BitwiseOperators {
+	
+	public static void main(String[] args)  {
+		
+		
+//		1101 -> 0110 -> 0011 ->0001 ->0000 rightshift(>>) leftshift(<<)
+		
+//		10 -> 1010
+//		13 -> 1101
+//	OrOperation15   ->  1111
+//		|- pipesign and or 
+	
+//		int a = 10;
+//		int b = 13;
+		
+//		int c = b << 3;
+//		
+//		System.out.println(c);
+		
+//		int a = 4;  asignmentoperator
+//		a += 2;  a = a + 2;
+//		a-= 2;
+//		a %= 2;
+//		a/= 2;
+//		a *= 2;
+//		a<<= 2;
+//		a>>= 2;
+//		a &= 2;
+		a |= 2;
+		
+		System.out.println(a);   
+		
+	}
+
+}
