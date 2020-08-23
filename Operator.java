ae894675aa69580086ce56b857b13e8f6341818b
Yashwant kaushal <54852557+yashwantkaushal@users.noreply.github.com>
2020-08-23 23:19:02 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,25 @@
+package operator;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		
+		int a = 23;
+		int b = 45;
+//		
+//		int c = a * a + b * b + 2 * b * a;
+//		
+//		int d = (a + b) * (a + b);
+//		
+//		System.out.println(c);
+		
+//		double c = (double)b / (double)a;
+		
+//		int c = b % a;
+		
+//		int c = b++;
+		int c = --b;
+		System.out.println(c +" " + b);   
+	}
+
+}
