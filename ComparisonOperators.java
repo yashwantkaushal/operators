5274593917d71c1b0c8ecf29dab4804d94475dc8
Yashwant kaushal <54852557+yashwantkaushal@users.noreply.github.com>
2020-08-23 23:18:14 +0530

    Add files via upload

---
+++ ComparisonOperators.java
@@ -0,0 +1,12 @@
+package operator;
+
+public class ComparisonOperators {
+	public static void main(String[] args) {
+		
+		int a = 6;
+		int b = 6;
+		
+//		System.out.println(a!=b);(<,>,<=,>=,!=,==)comparisonoperator
+	}
+
+}
